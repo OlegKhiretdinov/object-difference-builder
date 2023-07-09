176aa0b2a4d89c360b92ee3a434fde143826dd02
OlegKhiretdinov <oleg@oleg.win>
2023-07-09 20:02:03 +0300

    test: исправленны тесты

--- tests/test_get_dict_diff.py
+++ tests/test_get_dict_diff.py
@@ -1,13 +1,7 @@
 from gendiff.get_dict_diff import get_dict_diff
 from tests.fixtures.dict_example import dict_1, dict_2
+from tests.fixtures.diff_eample import diff_example
 
 
-test_file = open('tests/fixtures/simple_obj.txt', 'r')
-test_data = test_file.read()
-test_file.close()
-
-
-def test_diff_json():
-    result = get_dict_diff(dict_1, dict_2)
-    # assert result == test_data
-    assert True
+def test_get_dict_diff():
+    assert get_dict_diff(dict_1, dict_2) == diff_example
