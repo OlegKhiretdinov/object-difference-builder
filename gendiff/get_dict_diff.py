b17022a008cc45402358071fac1408ceb4624ab9
OlegKhiretdinov <oleg@oleg.win>
2023-08-08 20:33:53 +0300

    refactor: объеденены два цикла

--- gendiff/get_dict_diff.py
+++ gendiff/get_dict_diff.py
@@ -54,43 +54,41 @@ def get_changed_values(name, dict_1, dict_2):
 
 
 def get_dict_diff(dict_1, dict_2):
-    keys1 = list(dict_1)
-    keys2 = list(dict_2)
-    keys1.sort()
-    keys2.sort()
+    keys = set(list(dict_1) + list(dict_2))
     dict_dif = []
 
-    for key in keys1:
+    for key in keys:
         prop = {"key": key}
-        is_value_dict = isinstance(dict_1[key], dict)
-        if key not in keys2:
+        # ключа нет во втором словаре
+        if key not in dict_2:
+            is_value_dict = isinstance(dict_1[key], dict)
             prop["status"] = DELETED
             match is_value_dict:
                 case True:
                     prop["values"] = get_dict_diff(dict_1[key], dict_1[key])
                 case False:
                     prop["values"] = {"initial": dict_1[key]}
+        # ключа нет в первом словаре
+        elif key not in dict_1:
+            prop["status"] = ADDED
+            prop["values"] = get_dict_diff(dict_2[key], dict_2[key]) \
+                if isinstance(dict_2[key], dict) \
+                else {"current": dict_2[key]}
+        # ключи есть в обоих словарях БЕЗ изменении
         elif dict_1[key] == dict_2[key]:
+            is_value_dict = isinstance(dict_1[key], dict)
             prop["status"] = PRISTINE
             match is_value_dict:
                 case True:
                     prop["values"] = get_dict_diff(dict_1[key], dict_2[key])
                 case False:
                     prop["values"] = {"initial": dict_1[key]}
+        # ключи есть в обоих словарях ЕСТЬ изменения
         else:
             prop["status"] = CHANGED
             prop["values"] = get_changed_values(key, dict_1, dict_2)
 
         dict_dif.append(prop)
-
-    for key in keys2:
-        if key not in keys1:
-            prop = {"key": key}
-            prop["status"] = ADDED
-            prop["values"] = get_dict_diff(dict_2[key], dict_2[key])\
-                if isinstance(dict_2[key], dict)\
-                else {"current": dict_2[key]}
-            dict_dif.append(prop)
 
     dict_dif.sort(key=lambda item: item["key"])
 
